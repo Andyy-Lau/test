3a0c501ff7ce40f37890fb1a4a785b2639924296
Andyy-Lau <2005andyl@gmail.com>
2023-03-01 13:37:13 -0500

    initiated bikes, about and cart pages

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function redirect() {
-    location.href= "file:///C:/Users/2005a/Desktop/ICS4U/site.html";
+    location.href= "C:/Users/2005a/Desktop/ICS4U/site.html";
 }
---
+++ sites.js
@@ -0,0 +1,11 @@
+function redirectBikes() {
+    location.href = "C:\Users\2005a\Desktop\ICS4U\bikes.html"
+}
+
+function redirectAbout() {
+    location.href = "C:\Users\2005a\Desktop\ICS4U\about.html"
+}
+
+function redirectCart() {
+    location.href = "C:\Users\2005a\Desktop\ICS4U\cart.html"
+}
