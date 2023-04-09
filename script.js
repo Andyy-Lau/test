02e8107ebdd2a85ea5c0ac50addfe25f9421a8c5
Andyy-Lau <2005andyl@gmail.com>
2023-04-08 22:17:59 -0400

    Created gitignore file

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const TMDB_API_KEY = "7d343ae967a862c86915cc4c15444dcb"
+import { TMDB_API_KEY } from "./key.js";
 
 const getTMDBData = async (url) => {
-    return (await axios.get(url).data);
-}
+  return await axios.get(url).data;
+};
 
-let movieData = await getTMDBData(`https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`);
+let movieData = await getTMDBData(
+  `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`
+);
 console.log(movieData);
