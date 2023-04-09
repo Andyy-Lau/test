37c0ebbc8f25874e88754f8c3a1a0ee3a380b306
Andyy-Lau <2005andyl@gmail.com>
2023-04-08 22:46:59 -0400

    Successfully sent a query to API

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import { TMDB_API_KEY } from "./key.js";
 
 const getTMDBData = async (url) => {
-  return await axios.get(url).data;
+  return (await axios.get(url)).data;
 };
 
 let movieData = await getTMDBData(
-  `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`
+  `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&adult=false`
 );
 console.log(movieData);
