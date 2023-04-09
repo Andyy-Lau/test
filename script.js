41ea88ed9351efa2382e70093180bd574feea697
Andyy-Lau <2005andyl@gmail.com>
2023-04-08 23:05:35 -0400

    Pulled out the results from the query

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ let movieData = await getTMDBData(
   `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&adult=false`
 );
 console.log(movieData);
+
+movieData.results.forEach((movie) => {
+  console.log(`movie title: ${movie.title}, \n\npopularity: ${movie.popularity}, \n\noverview: ${movie.overview}, \n\nid: ${movie.id}`);
+});
