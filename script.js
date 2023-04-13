f8d6a39c89d9acab06f5d1ef2c1e278a2ee8df77
Andyy-Lau <2005andyl@gmail.com>
2023-04-13 14:39:06 -0400

    implemented js

--- script.js
+++ script.js
@@ -5,54 +5,78 @@ const getTMDBData = async (url) => {
   return (await axios.get(url)).data;
 };
 
-const createMovieContainer = (title, popularity, overview, backdrop, poster) => {
-    const container = document.createElement("div");
-    const text = document.createElement("div");
-    const h1 = document.createElement("h1");
-    const h2 = document.createElement("h2");
-    const h3 = document.createElement("h3");
-    const movieBackdrop = document.createElement("img");
-    const movieImg = document.createElement("img");
-    const trailerButton = document.createElement("button");
+const createMovieContainer = (id, title, popularity, overview, poster) => {
+  const container = document.createElement("div");
+  const text = document.createElement("div");
+  const movieTitle = document.createElement("h1");
+  const movieOverview = document.createElement("h2");
+  const moviePopularity = document.createElement("h3");
+  const movieImg = document.createElement("img");
+  const trailerButton = document.createElement("button");
 
-    container.classList.add("container");
-    movieImg.src = `https://image.tmdb.org/t/p/original/${poster}`;
-    movieBackdrop.src = `${backdrop}`;
-    h1.innerText = title;
-    h2.innerText = overview;
-    h3.innerText = popularity;
-    trailerButton.innerText = "Get";
+  container.classList.add("container");
+  text.classList.add("description");
+  movieImg.src = `https://image.tmdb.org/t/p/original/${poster}`;
+  movieTitle.innerText = title;
+  movieOverview.innerText = overview;
+  moviePopularity.innerText = popularity;
+  trailerButton.innerText = "Trailer";
 
-    text.append(h1);
-    text.append(h2);
-    text.append(h3);
+  trailerButton.addEventListener("click", async () => {
+    const trailersData = await getTMDBData(
+      `https://api.themoviedb.org/3//movie/${id}/videos?api_key=${TMDB_API_KEY}&language=en-US&adult=false`
+    );
 
-    container.append(movieImg);
-    container.append(text);
-    container.append(movieBackdrop);
-    container.append(trailerButton);
-}
+    console.log(trailersData);
 
+    const trailer = trailersData.results.filter((trailer) => {
+      return trailer.type === "Trailer";
+    });
+
+    !trailer.length
+      ? alert("Sorry! No trailers for this film.")
+      : window.open(`https://www.youtube.com/watch?v=${trailer.at(0).key}`);
+  });
+
+  text.append(movieTitle);
+  text.append(movieOverview);
+  text.append(moviePopularity);
+
+  container.append(movieImg);
+  container.append(text);
+  container.append(trailerButton);
+
+  return container;
+};
 
 async function getMovie(id) {
   let movie = await getTMDBData(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${TMDB_API_KEY}&language=en-US&adult=false`
   );
-  createMovieContainer(movie.title, movie.popularity, movie.overview, movie.backdrop_path, movie.poster_path)
+  console.log(movie);
+  console.log(movie.id);
+  console.log(movie.title);
+  const container = createMovieContainer(
+    movie.id,
+    movie.title,
+    movie.popularity,
+    movie.overview,
+    movie.poster_path
+  );
+  movies.appendChild(container);
 }
 
 function clearMovie() {
   let isEmpty = document.getElementById("movies").innerHTML === "";
   if (isEmpty) {
-    return
+    return;
   } else {
     document.getElementById("movies").innerHTML = "";
   }
 }
 
 document.getElementById("get").addEventListener("click", (e) => {
-  clearMovie()
+  clearMovie();
   let id = document.getElementById("options").value;
   getMovie(id);
 });
-
