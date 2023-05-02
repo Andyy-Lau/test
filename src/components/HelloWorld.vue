f418c3cf6f2517494c15dde549e9a738d7dfa2a0
Andyy-Lau <2005andyl@gmail.com>
2023-05-02 13:46:58 -0400

    created vite project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,34 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import axios from "axios";
 
-defineProps({
-  msg: String,
-})
+const movie = ref("movie")
 
-const count = ref(0)
+const getMovie = async () => {
+  let data = (await axios.get(`https://api.themoviedb.org/3/movie/${movie.value}?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US&adult=false`)).data
+  console.log(data)
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <header>
+  </header>
+  <div class="selector">
+    <select v-model="movie" @change="getMovie">
+      <option value="333952">Teletubbies: Musical Playtime</option>
+        <option value="260984">Teletubbies: Dance with the Teletubbies</option>
+        <option value="265794">Thomas and Friends: Santa's Little Engine</option>
+        <option value="838424">Thomas and Friends 1945</option>
+        <option value="221029">Thomas and Friends: King of the Railway</option>
+        <option value="221029">Thomas and Friends: The Great Discovery</option>
+        <option value="292821">Thomas and Friends: Tale of the Brave</option>
+        <option value="305470">Power Rangers</option>
+        <option value="9070">Mighty Morphin Power Rangers</option>
+        <option value="447404">Pokémon Detective Pikachu</option>
+    </select>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div class="movie"></div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
