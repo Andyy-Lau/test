76585703f74af6fa3111bfe9aaeaccd5d304b61d
Andyy-Lau <2005andyl@gmail.com>
2023-05-24 13:53:13 -0400

    movie page:sent a get request

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,9 +1,18 @@
 <script setup>
     import { ref } from "vue"
+    import axios from "axios"
 
-    const getMovie = async(id) => {
-        await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US&adult=false`)
+    let movieIds = []
+
+    const getMovie = async() => {
+        const movieResults = (await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US&adult=false`)).data.results
+        movieResults.map(movie => {
+            movieIds.push(movie.id)
+        })
+        return movieResults
     }
+    console.log(getMovie())
+    console.log(movieIds)
 </script>
 
 <template>
